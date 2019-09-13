db9196a45cf59f9bcc5a94a616039c5988a013db
philipkantola <philip.kantola@live.se>
2019-09-13 17:53:28 +0200

    Error handling included for faulty input
    Now error message shows up and the program keeps running if city that does not exist is entered, or if no input is made

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -15,7 +15,7 @@ class App extends React.Component {
     country: undefined,
     population: undefined, 
     error: undefined
-
+  
   }
   
 // function to fetch information about desired city from API 
@@ -26,13 +26,30 @@ class App extends React.Component {
     const data = await api_call.json();
     this.setCurrentPage("cityPage");
     console.log(data)
-  
-    this.setState({
-      ...this.state,
-      city: data.geonames[0].toponymName,
-      country: data.geonames[0].countryName,
-      population: data.geonames[0].population
-    })
+    // if nothing is put into the form
+    if(!city){
+      this.setCurrentPage("citySearchPage");
+    }
+    // if city does not exist in db
+    else if(data.totalResultsCount === 0){
+      this.setState({
+        ...this.state,
+        city: undefined,
+        country: undefined,
+        population: undefined,
+        error: "There exists no such city in our database, try again"
+      })
+      this.setCurrentPage("citySearchPage");
+    } else{
+      // if city exists in db
+      this.setState({
+        ...this.state,
+        city: data.geonames[0].toponymName,
+        country: data.geonames[0].countryName,
+        population: data.geonames[0].population, 
+        error: undefined
+      })
+    }
   
   }
 
@@ -48,7 +65,7 @@ setCurrentPage = (name) => {
       <div> 
         <h1> City Pop </h1>
         {this.state.currentPage === "home" && <Home onClickCitySearchBtn ={this.setCurrentPage} onClickCountrySearchBtn = {this.setCurrentPage}/>}
-        {this.state.currentPage === "citySearchPage" && <CitySearch getCity = {this.getCity} />}
+        {this.state.currentPage === "citySearchPage" && <CitySearch getCity = {this.getCity} error = {this.state.error} />}
         {this.state.currentPage === "countrySearchPage" && <CountrySearch />}
         {this.state.currentPage === "cityPage" && <CityPage 
           city = {this.state.city} 
--- src/components/citySearch/citySearch.js
+++ src/components/citySearch/citySearch.js
@@ -10,6 +10,7 @@ class CitySearch extends React.Component {
         <div>
          <h2>SEARCH BY CITY</h2>
          <SearchBar getCity = {this.props.getCity} />
+         {this.props.error && <p> {this.props.error} </p>}
         </div>
       );
     }
